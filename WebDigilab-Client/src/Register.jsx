c2162a95bf55b68b8a417779b22846568e6cc4f8
Evandita <evanditaw@gmail.com>
2024-05-26 00:37:28 +0700

    3d logo for register page

--- WebDigilab-Client/src/Register.jsx
+++ WebDigilab-Client/src/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 import Spline from "@splinetool/react-spline";
 
 function Register() {
@@ -11,6 +12,7 @@ function Register() {
     const [Email, setEmail] = useState('');
     const [Password, setPassword] = useState('');
     const [PasswordConfirmation, setPasswordConfirmation] = useState('');
+    const [loading, setLoading] = useState(true);
 
     const handleCreateAccountAslab = (event) => {
         
@@ -122,18 +124,21 @@ function Register() {
         <section className="bg-white">
             <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
                 <aside className="relative block h-16 lg:order-first lg:col-span-5 lg:h-full xl:col-span-6">
-                    <img
+                    {loading && (
+                        <img
                         alt=""
                         src="../public/embedded-system-micro.jpg"
                         className="absolute inset-0 h-full w-full object-cover"
+                        />
+                    )}
+                    <Spline
+                        scene="https://prod.spline.design/bHyEsq69KAznj0Ub/scene.splinecode"
+                        onLoad={() => setLoading(false)}
                     />
                 </aside>
 
                 <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
                     <div className="max-w-xl lg:max-w-3xl">
-                        <div className="flex justify-center items-center h-40 w-40">
-                            <Spline scene="https://prod.spline.design/GzRnCGNlgrVZJUS4/scene.splinecode" />
-                        </div>
 
                         <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
                             Register your DigiLab Account
