994d397a56002f52bb63e590479dc88835ef435a
Evandita <evanditaw@gmail.com>
2024-06-09 18:07:41 +0700

    add readme and update contact

---
+++ README.md
@@ -0,0 +1,67 @@
+# Digital Laboratory Web (DigiWeb) Project
+
+## Description
+
+...
+
+## How to Use
+- Download or clone the repository  
+```git clone https://github.com/SistemBasisData2024/WebDigilab```
+
+- Install dependencies    
+```npm install```
+
+- Run the backend  
+```npm run start```
+
+- Run the frontend  
+```npm run dev```
+
+## Web Preview
+
+1. **Login page:** Halaman ini digunakan bagi pengguna yang ingin masuk ke WebDigilab dengan akun yang dimilikinya. Pengguna dapat masuk sebagai Asisten atau Praktikan
+
+![Login](img/Login.png)
+
+
+2. **Register page:** Halaman ini digunakan bagi pengguna yang ingin masuk ke WebDigilab dengan membuat akun baru.
+
+![Register](img/Register.png)
+
+3. **Home page:** Halaman ini merupakan halaman beranda yang menyambut pengguna yang masuk. Pada halaman ini pengguna dapat pergi ke halaman Courses bagi Praktikan dan Edit Courses khusus bagi Asisten.
+
+![Home](img/Home.png)
+
+4. **Courses page:** Halaman ini merupakan halaman bagi pengguna untuk menelusuri berbagai course yang dapat diakses olehnya. Pengguna dapat memilih sebuah course untuk pergi ke halaman course tersebut.
+
+![Courses](img/Courses.png)
+
+5. **Course page:** Halaman ini merupakan halaman bagi pengguna untuk membaca modul pada course yang telah dipilihnya.
+
+![Course](img/Course.png)
+
+6. **Edit Courses page:** Halaman ini merupakan halaman bagi Asisten untuk mengedit course, menambahkan course, dan menambahkan chapter yang ada pada WebDigilab.
+
+![Edit Courses](img/CoursesEdit.png)
+
+7. **Edit Course form:** Form ini digunakan oleh Asisten untuk mengedit course yang ada. 
+
+![Edit Course](img/CourseEdit.png)
+
+8. **Add Course form:** Form ini digunakan oleh Asisten untuk menambahkan sebuah course baru pada WebDigilab.
+
+![Add Course](img/CourseAdd.png)
+
+9.  **Add Chapter form:** Form ini digunakan oleh Asisten untuk menambahkan sebuah chapter baru pada WebDigilab.
+
+![Add Chapter](img/ChapterAdd.png)
+
+10. **Navigation:** Fitur ini berguna untuk memudahkan pengguna berpindah-pindah halaman dari Home, Courses, dan Edit Courses.
+
+![Navigation](img/Navigation.png)
+
+11. **Account Settings:** Halaman ini berguna untuk pengguna mengedit profile dari akunnya.
+
+![Account Settings](img/AccountSetting.png)
+
+
--- WebDigilab-Client/src/GetContact.jsx
+++ WebDigilab-Client/src/GetContact.jsx
@@ -1,71 +1,126 @@
 import React from 'react';
+import { CookiesProvider, useCookies } from 'react-cookie'
+import Navigation from './Navigation';
 
 // Data statis kontak pengajar
 const teachers = [
   {
     id: 1,
     name: 'Evandita',
+    angkatan: '2022',
     email: 'evan@gmail.com',
     photoUrl: 'https://via.placeholder.com/150',
-    department: 'Electrical Engineering',
-    phone: 'lineid',
+    bio: '',
+    whatsappNo: '+62',
+    lineId: 'lineid',
+    instagram: 'ig',
+    profileUrl: 'https://www.linkedin.com/'
   },
   {
     id: 2,
     name: 'Edgrant',
+    angkatan: '2022',
     email: 'edgrant@gmail.com',
     photoUrl: 'https://via.placeholder.com/150',
-    department: 'Electrical Engineering',
-    phone: 'lineid',
+    whatsappNo: '+62',
+    lineId: 'lineid',
+    instagram: 'ig',
+    profileUrl: 'https://www.linkedin.com/'
   },
   {
     id: 3,
     name: 'Giovan',
+    angkatan: '2022',
     email: 'gio@gmail.com',
     photoUrl: 'https://via.placeholder.com/150',
-    department: 'Electrical Engineering',
-    phone: 'lineid',
+    whatsappNo: '+62',
+    lineId: 'lineid',
+    instagram: 'ig',
+    profileUrl: 'https://www.linkedin.com/'
   },
-  // Tambahkan data pengajar lainnya di sini
 ];
 
 // Komponen untuk menampilkan satu kontak pengajar
 const TeacherContactCard = ({ teacher }) => {
   return (
-    <div className="max-w-sm w-full lg:max-w-full lg:flex shadow-lg rounded-lg overflow-hidden bg-gray-800">
-      <div className="flex-shrink-0">
-        <img className="w-full h-48 object-cover lg:w-48" src={teacher.photoUrl} alt={`Avatar of ${teacher.name}`} />
-      </div>
-      <div className="p-4 flex flex-col justify-between leading-normal">
-        <div className="mb-8">
-          <div className="text-white font-bold text-xl mb-2">{teacher.name}</div>
-          <p className="text-gray-400 text-base">{teacher.email}</p>
+    
+    <a
+      href={teacher.profileUrl}
+      className=" relative block bg-gray-700 overflow-hidden rounded-lg border border-gray-800 p-4 sm:p-6 lg:p-8"
+    >
+      <span
+        className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-blue-300 via-blue-500 to-purple-600"
+      ></span>
+
+      <div className="sm:flex sm:justify-between sm:gap-4">
+        <div>
+          <h3 className="text-lg font-bold text-white sm:text-xl">
+            {teacher.name}
+          </h3>
+
+          <p className="mt-1 text-xs font-medium text-gray-600">Angkatan: {teacher.angkatan}</p>
+          <p className="mt-1 text-xs font-medium text-gray-600">Email: {teacher.email}</p>
         </div>
-        <div className="flex items-center">
-          <div className="text-sm">
-            <p className="text-gray-400 leading-none">{teacher.department}</p>
-            <p className="text-gray-400">{teacher.phone}</p>
-          </div>
+
+        <div className="hidden sm:block sm:shrink-0">
+          <img
+            alt=""
+            src={teacher.photoUrl}
+            className="size-16 rounded-lg object-cover shadow-sm"
+          />
         </div>
       </div>
-    </div>
+
+      <div className="mt-4">
+        <p className="text-pretty text-sm text-gray-500">
+          {teacher.bio}
+        </p>
+      </div>
+
+      <dl className="mt-6 flex gap-4 sm:gap-6">
+        <div className="flex flex-col-reverse">
+          <dt className="text-sm font-medium text-gray-600">{teacher.whatsappNo}</dt>
+          <dd className="text-xs text-gray-500">Whatsapp No</dd>
+        </div>
+
+        <div className="flex flex-col-reverse">
+          <dt className="text-sm font-medium text-gray-600">{teacher.lineId}</dt>
+          <dd className="text-xs text-gray-500">Line id</dd>
+        </div>
+
+        <div className="flex flex-col-reverse">
+          <dt className="text-sm font-medium text-gray-600">{teacher.instagram}</dt>
+          <dd className="text-xs text-gray-500">Instagram</dd>
+        </div>
+      </dl>
+    </a>
+    
   );
 };
 
 // Komponen utama untuk halaman kontak pengajar
 function GetContact ()  {
+  const [cookies, setCookie] = useCookies('user')
+
   return (
-    <div className="bg-gray-900 min-h-screen">
+    <>
+    <Navigation isAslab={cookies.user.isAslab} />
+    <div className="bg-gray-900 min-h-screen ">
       <div className="container mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold mb-6 text-white">Aslab Contact</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-          {teachers.map((teacher) => (
-            <TeacherContactCard key={teacher.id} teacher={teacher} />
-          ))}
+        <h1 className="text-3xl font-bold mb-20 text-white text-center">Digital Laboratory Assistants</h1>
+        <div className="flex justify-center">
+          <div className="w-2/3">
+            <div className="grid grid-cols-1 gap-10">
+              {teachers.map((teacher) => (
+                <TeacherContactCard key={teacher.id} teacher={teacher} />
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
+    </>
   );
-};
+}
 
 export default GetContact;
